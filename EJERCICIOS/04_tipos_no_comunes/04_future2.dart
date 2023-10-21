355209bdac1bc4adad73698f0fc5aae011d6d5e1
josegt502 <68062175+josegt502@users.noreply.github.com>
2023-10-20 20:44:36 -0600

    Terminando Sección

---
+++ EJERCICIOS/04_tipos_no_comunes/04_future2.dart
@@ -0,0 +1,21 @@
+import 'dart:io';
+
+main() {
+  // OSX y Linux
+  // File file = new File(
+  //     Directory.current.path + '/04_tipos_nocomunes/assets/personas.txt');
+
+  // Windows
+  File file = new File(Directory.current.path +
+      '\\EJERCICIOS\\04_tipos_no_comunes\\assets\\personas.txt');
+
+  Future<String> f = file.readAsString();
+  // String f = file.readAsStringSync();
+
+  f.then((data) => print(data));
+  // f.then( print );
+
+  // print(f);
+
+  print('Fin del main');
+}
